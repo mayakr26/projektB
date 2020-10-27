7deb853dc0305fd4c2456831eb70e40a575b34ae
NadineKaa <nadine.kaldschmidt@t-online.de>
2020-10-27 12:53:04 +0100

    Initial

---
+++ src/views/Home.vue
@@ -0,0 +1,100 @@
+<template>
+<div id="home">
+    <div id="btn">
+        <b-button :to="{path: 'info'}">
+            <b-icon icon="card-text" aria-hidden="true" class="mr-1"></b-icon> Info
+        </b-button>
+    </div>
+    <br>
+    <h1>Wo muss ich eine Makse draußen tragen</h1>
+    <div id="map"></div>
+    <button id="asdasd" @click="exportGeo">Export</button>
+</div>
+</template>
+
+<script>
+import L from 'leaflet'
+import '@geoman-io/leaflet-geoman-free';
+import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css';
+
+export default {
+
+    data() {
+        return {
+            map: null
+        }
+    },
+
+    mounted() {
+        this.map = L.map('map').setView([53.56, 10], 14);
+        const tiles = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+            maxZoom: 20,
+            attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+        });
+
+        this.map.pm.addControls({
+            position: 'topleft',
+            drawCircle: false,
+        });
+
+        tiles.addTo(this.map);
+    },
+
+    methods: {
+        exportGeo() {
+            var collection = {
+                "type": "FeatureCollection",
+                "features": []
+            };
+
+            // Iterate the layers of the map
+            this.map.eachLayer(function (layer) {
+                // Check if layer is a marker
+                if (layer instanceof L.Marker || layer instanceof L.Polyline || layer instanceof L.Polygon) {
+                    // Create GeoJSON object from marker
+                    var geojson = layer.toGeoJSON();
+                    // Push GeoJSON object to collection
+                    collection.features.push(geojson);
+                }
+            });
+
+            console.log(collection)
+            console.log(JSON.stringify(collection))
+        }
+    }
+
+}
+</script>
+
+<style lang="scss">
+@import "~leaflet/dist/leaflet.css";
+
+#home {
+    h1 {
+        text-align: center
+    }
+
+    #map {
+        height: 80vh;
+        width: 100%
+    }
+
+    #btn {
+        text-align: left;
+        margin: 10px;
+    }
+
+    #asdasd {
+        position: fixed;
+        top: 0;
+        right: 0;
+        z-index: 10000;
+    }
+
+    #menu {
+        position: fixed;
+        top: 0;
+        left: 0;
+    }
+}
+</style>
---
+++ src/views/Info.vue
@@ -0,0 +1,51 @@
+<template>
+<div id="info">
+    <div id="btn">
+        <b-button :to="{path: '/'}">
+            <b-icon icon="map" aria-hidden="true" class="mr-1"></b-icon> Map
+        </b-button>
+    </div>
+    <h1>Info</h1>
+    <div id="infotext">
+        <p> Auf dem <strong>Steindamm</strong> im räumlichen Bereich von der Hausnummer 33 bis zum <strong>Steintorplatz </strong>, täglich von <em>12 Uhr bis 22 Uhr</em></p>
+        <p> In der <strong>Stralsunder Straße</strong>, täglich von <em>12 Uhr bis 22 Uhr </em></p>
+        <p> Auf dem <strong>Steintorplatz</strong> einschließlich der angrenzenden öffentlichen Wege, Straßen und Plätze, abgegrenzt durch die Bahnüberführung der Straße Steintordamm, dem Gebäude des Museums für Kunst und Gewerbe, dem Gebäude des Zentralen Omnibusbahnhofs, dem Gebäude mit der Hausnummer Steindamm 2, dem Gebäude mit der Hausnummer Steindamm 1, den Gebäuden mit den Hausnummern Steintorplatz 3 und Kirchenallee 57 sowie dem Gebäude des Hauptbahnhofs, täglich von <em>12 Uhr bis 22 Uhr</em> </p>
+        <p> Auf dem <strong>Ballindamm</strong> im räumlichen Bereich vor dem Gebäude mit der Hausnummer 40, abgegrenzt durch die Straßen Ballindamm und Bergstraße, täglich von <em>15 Uhr bis 18 Uhr</em></p>
+        <p> In der Straße <strong>Große Freiheit</strong> im räumlichen Bereich von der Hausnummer 1 bis zur Hausnummer 47, freitags, sonnabends sowie an Feiertagen und tags zuvor, jeweils von <em>18 Uhr bis 4 Uhr am Folgetag </em></p>
+        <p> In der Straße <strong>Hamburger Berg</strong> im räumlichen Bereich der Hausnummern 1 bis 39, freitags, sonnabends sowie an Feiertagen und tags zuvor, jeweils von <em>18 Uhr bis 4 Uhr am Folgetag</em></p>
+        <p> In der <strong>Talstraße</strong> im räumlichen Bereich der Hausnummern 1 bis 36, freitags, sonnabends sowie an Feiertagen und tags zuvor, jeweils von <em>18 Uhr bis 4 Uhr am Folgetag </em></p>
+        <p> Auf dem <strong>Hans-Albers-Platz </strong>einschließlich der <strong>Friedrichstraße</strong> im räumlichen Bereich zwischen und einschließlich den Hausnummern 11 beziehungsweise 24 bis 21 beziehungsweise 28, freitags, sonnabends sowie an Feiertagen und tags zuvor, jeweils von <em>18 Uhr bis 4 Uhr am Folgetag </em></p>
+        <p> Auf der Straße <strong>Reeperbahn</strong> einschließlich der Plätze <strong>Nobistor und Spielbudenplatz</strong>, abgegrenzt durch den Millerntorplatz, die Straße Zirkusweg, die Holstenstraße und den Finkenpark sowie in der Straße Spielbudenplatz im räumlichen Bereich der Hausnummern 1 bis 31, freitags, sonnabends sowie an Feiertagen und tags zuvor, jeweils von <em>18 Uhr bis 4 Uhr am Folgetag </em></p>
+        <p> Auf der Straße <strong>Schulterblatt</strong> im räumlichen Bereich zwischen den Straßen Susannenstraße und Rosenhofstraße, freitags, sonnabends sowie an Feiertagen und tags zuvor, jeweils von <em>20 Uhr bis 24 Uhr </em></p>
+        <p> Auf dem <strong>Alma-Wartenberg-Platz</strong> einschließlich der Bahrenfelder Straße im räumlichen Bereich zwischen und einschließlich den Hausnummern 135 beziehungsweise 146 und den Hausnummern 183 beziehungsweise 188, der Kleinen Rainstraße im räumlichen Bereich bis zu und einschließlich den Hausnummern 3 beziehungsweise 6, der Nöltingstraße im räumlichen Bereich bis zu und einschließlich den Hausnummern 5 beziehungsweise 12, der Friedensallee im räumlichen Bereich bis zu und einschließlich den Hausnummern 7 beziehungsweise 14 sowie der Bergiusstraße im räumlichen Bereich bis zu der Hausnummer 7, freitags und sonnabends von <em>19 Uhr bis 3 Uhr am Folgetag</em> </p>
+        <p> In der Straße <strong>Hohenesch</strong> im räumlichen Bereich von und einschließlich den Hausnummern 1 beziehungsweise 6 bis zur Bahrenfelder Straße, freitags und sonnabends von <em>19 Uhr bis 3 Uhr am Folgetag </em></p>
+        <p> In der Straße <strong>Mühlenkamp</strong> im räumlichen Bereich zwischen der Körnerstraße und der Preystraße, täglich von <em>12 Uhr bis 1 Uhr am Folgetag </em></p>
+        <p> Auf den <strong>Sankt-Pauli-Landungsbrücken</strong> einschließlich der dort befindlichen Pontonanlage und den Brücken 1 bis 10, montags bis freitags jeweils von 6 Uhr bis 18 Uhr sowie sonnabends und sonntags jeweils von <em>11 Uhr bis 18 Uhr</em> </p>
+        <p> (Stand: 10.10.2020) </p>
+    </div>
+</div>
+</template>
+
+<style lang="scss">
+#info {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+
+    h1 {
+        display: block;
+        text-align: center
+    }
+
+    #infotext {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        margin: 20px;
+        text-align: left;
+        color: #2c3e50;
+    }
+
+    #btn {
+        text-align: left;
+        margin: 10px
+    }
+}
+</style>
