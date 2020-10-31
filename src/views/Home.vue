7e26a7bbd4a7f76a4f40a448155af8f8d4dbb15b
Maya Kröger <maya.kroeger@haw-hamburg.de>
2020-10-31 15:24:11 +0100

    darkModeMap

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,7 +27,7 @@ export default {
 
     mounted() {
         this.map = L.map('map').setView([53.56, 10], 14);
-        const tiles = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+        const tiles = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
             maxZoom: 20,
             attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
         });
